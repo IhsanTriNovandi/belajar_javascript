aee42b80dee7f3eca489653295735cec72f3a9b2
IhsanTriNovandi <ihsantri22@gmail.com>
2024-10-01 20:48:56 +0700

    first commit

---
+++ 2main.js
@@ -0,0 +1,6 @@
+
+    const namaHari = ['minggu', 'senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu']
+    const hari = new Date().getDay()
+    const hariIni = namaHari[hari]
+
+    console.log(`hari ini adalah hari ${hariIni}`)
